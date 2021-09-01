86da7deac2b401b8a547452c16aadcc3e95d0e19
ManasRaut <msraut2002@gmail.com>
2021-09-01 18:59:28 +0530

    completed inventory class, taking order, making copy of files features

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from inventory import Inventory as inventory
-from cli import CLI as cli
+from inventory import Inventory
+from cli import CLI
+import os
+from pathlib import Path
+import time
 
 dashboard = '''
 ---------------------------------------------------
 Inventory Management System
 
-1. 1. Place order
+1. Place order
 2. stock up inventory
 3. History
 4. view inventory copy
@@ -13,6 +16,11 @@ Inventory Management System
 ---------------------------------------------------
 '''
 
+def printError():
+    print("\n*******ERROR******\n")
+
+inventory = Inventory()
+cli = CLI()
 cmd = -1
 # main while loop
 while cmd != 5:
@@ -20,13 +28,51 @@ while cmd != 5:
     cmd = int(input('> '))
 
     if cmd == 1:
-        pass
+        inventory.read_records()
+        id = input("Enter id: ")
+        qty = int(input("Enter quantity: "))
+        result, order = inventory.handle_order(id, qty)
+
+        if result == "OUTOFSTOCK": # done
+            print("Oops, sorry out of stock!!!")
+            continue
+        elif result == "FEWINSTOCK": # done
+            print('''
+            Requested items: {}
+            Items in stock: {}
+            '''.format(order["qty"], order["instock"]))
+            ans = input("Do you want to buy remaining {} items ? (Y or N)".format(order["instock"]))
+            if ans == "N":
+                print("OK order cancelled")
+                continue
+            elif ans == "Y":
+                order["qty"] = order["instock"]
+        elif result == "NOITEMPRESENT": # incomplete
+            print("No Item Exists")
+            continue
+
+        res = inventory.deduct_inventory(order)
+        res &= inventory.update_records()
+        if res:
+            print(inventory.get_bill(order))
+        else:
+            printError()
+            
     elif cmd == 2:
         pass
     elif cmd == 3:
         pass
     elif cmd == 4:
-        pass
+        home = Path.home()
+        name = (time.ctime() + ".json").replace(":", "-")
+        path = os.path.join(home, "Documents", "InventorySystem")
+        if not os.path.exists(path):
+            os.makedirs(path)
+        fh = open(path+"\\"+name, "w")
+        fh.write(open("records.json", "r").read())
+        fh.close()
+        os.startfile(path+"\\"+name)
+        print("Saved inventory copy", path+"\\"+name)
     else:
         pass
 
--- inventory.py
+++ inventory.py
@@ -1,3 +1,63 @@
+import json
+
 class Inventory():
     def __init__(self):
-        pass
+        self.records = []
+    
+    def read_records(self):
+        fh = open('records.json', "r")
+        self.records = json.loads(fh.read())
+        fh.close()
+    
+    def handle_order(self, id, qty):
+        itm = None
+        order = None
+        for item in self.records:
+            if item["id"] == id:
+                order = item.copy()
+                order["instock"] = order["qty"]
+                order["qty"] = qty
+                itm = item
+                break
+        if itm:
+            if itm["qty"] == 0:
+                return ("OUTOFSTOCK", order)
+            elif itm["qty"] < order["qty"]:
+                return ("FEWINSTOCK", order)
+            else:
+                return ("INSTOCK", order)
+        else:
+            return ("NOITEMPRESENT", order)
+    
+    def update_records(self):
+        fh = open('records.json', "w")
+        try:
+            fh.write(json.dumps(self.records, indent = 4))
+            fh.close()
+            self.read_records()
+            return True
+        except:
+            return False
+
+    def deduct_inventory(self, order):
+        for item in self.records:
+            if item["id"] == order["id"]:
+                item["qty"] = item["qty"] - order["qty"]
+                return True
+        return False
+
+    def get_bill(self, order):
+        bill = '''
+        ***************************************************
+        Bill No.\t{}
+        Item Id:\t{}
+        Name:\t{}
+        Price:\t{}
+
+        Requested Quantity:\t{}
+        ---------------------------------------------------
+        Total Amount:\tRs. {}
+        ***************************************************
+        '''.format("temp", order["id"], order["name"], order["price"], order["qty"], order["qty"] * order["price"])
+
+        return bill
